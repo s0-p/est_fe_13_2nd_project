bd77ac74e33ec5dfef1728f3af3717109a474ff0
s0-p <b00v0429@gmail.com>
2026-06-16 15:49:52 +0900

    feat: 상품 load

--- js/product-list.js
+++ js/product-list.js
@@ -1,6 +1,6 @@
 import headerModule from './components/header.js';
+import createProductCard from './components/product-card.js';
 headerModule();
-
 // Fillters
 const filters = document.querySelector('.filters');
 
@@ -65,3 +65,46 @@ filterApplyBtn.addEventListener('click', () => {
   filterModal.classList.remove('display_flex');
   filterModal.classList.add('display_none');
 });
+const productList = document.querySelector('.product_list');
+const DATA_PATH = '/data/products.json';
+const LIMIT = 12;
+let total = 0;
+let skip = 0;
+
+async function loadProducts() {
+  try {
+    const res = await fetch(DATA_PATH);
+    if (!res.ok) throw new Error(`DATA_PATH:${res.status}`);
+    const products = await res.json();
+    total = products.length;
+    console.log(total);
+    for (let i = skip; i < skip + LIMIT; i++) {
+      console.log(createProductCard(products[i]));
+      let card = createProductCard(products[i]);
+
+      // Swiper Slide
+      let swiper = new Swiper(card.querySelector('.image_slider'), {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        navigation: {
+          nextEl: card.querySelector('.image_next'),
+          prevEl: card.querySelector('.image_prev'),
+        },
+        pagination: {
+          el: card.querySelector('.read_only_pagers'),
+          clickable: false,
+        },
+        mousewheel: true,
+        keyboard: true,
+      });
+      productList.append(card);
+    }
+
+    skip += LIMIT;
+  } catch (err) {
+    console.log(err);
+  } finally {
+  }
+}
+loadProducts();
