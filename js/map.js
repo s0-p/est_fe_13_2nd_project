bbc2b5d10d9ccb457203d186476019e4e30309ed
rkskek8484-cell <rkskek8484@gmail.com>
2026-06-17 11:47:38 +0900

    feat: API연동

---
+++ js/map.js
@@ -0,0 +1,63 @@
+var mapContainer = document.getElementById('#map'), // 지도를 표시할 div
+  mapOption = {
+    center: new kakao.maps.LatLng(37.4935506, 127.0310534), // 지도의 중심좌표
+    level: 3, // 지도의 확대 레벨
+  };
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 지도 레벨을 표시합니다
+displayLevel();
+
+// 지도 레벨은 지도의 확대 수준을 의미합니다
+// 지도 레벨은 1부터 14레벨이 있으며 숫자가 작을수록 지도 확대 수준이 높습니다
+function zoomIn() {
+  // 현재 지도의 레벨을 얻어옵니다
+  var level = map.getLevel();
+
+  // 지도를 1레벨 내립니다 (지도가 확대됩니다)
+  map.setLevel(level - 1);
+
+  // 지도 레벨을 표시합니다
+  displayLevel();
+}
+
+function zoomOut() {
+  // 현재 지도의 레벨을 얻어옵니다
+  var level = map.getLevel();
+
+  // 지도를 1레벨 올립니다 (지도가 축소됩니다)
+  map.setLevel(level + 1);
+
+  // 지도 레벨을 표시합니다
+  displayLevel();
+}
+
+function displayLevel() {
+  var levelEl = document.getElementById('maplevel');
+  levelEl.innerHTML = '현재 지도 레벨은 ' + map.getLevel() + ' 레벨 입니다.';
+}
+
+// 마커가 표시될 위치입니다
+var markerPosition = new kakao.maps.LatLng(37.4935506, 127.0310534);
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+  position: markerPosition,
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
+var iwContent =
+    '<div style="padding:5px;">Hello World! <br><a href="https://map.kakao.com/link/map/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/Hello World!,37.4935506,127.0310534" style="color:blue" target="_blank">길찾기</a></div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+  iwPosition = new kakao.maps.LatLng(33.450701, 126.570667); //인포윈도우 표시 위치입니다
+
+// 인포윈도우를 생성합니다
+var infowindow = new kakao.maps.InfoWindow({
+  position: iwPosition,
+  content: iwContent,
+});
+
+// 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+infowindow.open(map, marker);
